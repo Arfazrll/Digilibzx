613d4786a13bed1e904e1c6323debca76b1412e5
Arfazrll <azril4974@gmail.com>
2025-12-06 00:38:02 +0700

    Update

---
+++ backend/src/main/java/com/digilibz/dto/user/StudentRequestDTO.java
@@ -0,0 +1,30 @@
+package com.digilibz.dto.user;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+public class StudentRequestDTO extends UserRequestDTO {
+    @NotBlank(message = "NIM is required for student")
+    @Schema(description = "NIM of the student", example = "1301223164")
+    private String nim;
+
+    @NotBlank(message = "Year is required for students")
+    @Schema(description = "Tahun angkatan", example = "2022")
+    private String year;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
