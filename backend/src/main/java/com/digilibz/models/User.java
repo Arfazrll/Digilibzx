fc840705b766ba4e8a49796364dcd419dc3f9b82
Arfazrll <azril4974@gmail.com>
2025-12-06 11:19:08 +0700

    Update

---
+++ backend/src/main/java/com/digilibz/models/User.java
@@ -0,0 +1,123 @@
+package com.digilibz.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "email"),
+                @UniqueConstraint(columnNames = "phone"),
+                @UniqueConstraint(columnNames = "nim"),
+                @UniqueConstraint(columnNames = "nip")
+        })
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(unique = true)
+    private String phone;
+
+    @Column(unique = true)
+    private String nim;
+
+    @Column(unique = true)
+    private String nip;
+
+    private String year;
+
+    public enum Role {
+        admin,
+        student,
+        ADMIN, lecturer
+    }
+
+    public User() {}
+}
---
+++ backend/src/main/java/com/digilibz/service/user/UserService.java
@@ -0,0 +1,60 @@
+package com.digilibz.service.user;
+
+import com.digilibz.models.User;
+import com.digilibz.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    protected UserRepository userRepository;
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
+
+    public Optional<User> findById(String id) {
+        return Optional.ofNullable(userRepository.findById(id).orElse(null));
+    }
+
+    public List<User> findAll(String role) {
+        if (role != null && !role.isEmpty()) {
+            return userRepository.findAllByRole(User.Role.valueOf(role));
+        }
+        return userRepository.findAll();
+    }
+
+    public void deleteById(String id) {
+        userRepository.deleteById(id);
+    }
+
+    @Lazy
+    @Autowired
+    private AdminService adminService;
+    @Lazy
+    @Autowired
+    private LecturerService lecturerService;
+    @Lazy
+    @Autowired
+    private StudentService studentService;
+
+    public User updateUser(User newUser, String id) {
+        switch (newUser.getRole()) {
+            case admin:
+                return adminService.updateAdmin(newUser, id);
+            case lecturer:
+                return lecturerService.updateLecturer(newUser, id);
+            case student:
+                return studentService.updateStudent(newUser, id);
+            default:
+                throw new IllegalArgumentException("Invalid user role");
+        }
+    }
+}
